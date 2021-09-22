7c2a2a04f6d7f9399e817891c72997bdea328534
wujing <wujing50@huawei.com>
2021-09-22 20:28:20 +0800

    Unit test build optimization 1. cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_UT=ON -DENABLE_GCOV=ON  .. 2. make -j $(nproc) 3. make test [or ctest] 4. make coverage
    Signed-off-by: wujing <wujing50@huawei.com>

--- tests/libocispec_ut.cpp
+++ tests/libocispec_ut.cpp
@@ -18,7 +18,7 @@
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
  ********************************************************************************/
 #include <gtest/gtest.h>
 
@@ -27,77 +27,74 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "read_file.h"
 #include "oci_runtime_hooks.h"
+#include "read_file.h"
 
-TEST(libocispec_testcase, test_oci_runtime_spec_hooks)
-{
-    const char *fname = "./ocihook.json";
-    oci_runtime_spec_hooks *hooks = nullptr;
-    parser_error jerr = nullptr;
-    char *jstr = nullptr;
+TEST(libocispec_testcase, test_oci_runtime_spec_hooks) {
+  const char *fname = "./ocihook.json";
+  oci_runtime_spec_hooks *hooks = nullptr;
+  parser_error jerr = nullptr;
+  char *jstr = nullptr;
 
-    hooks = oci_runtime_spec_hooks_parse_file(fname, nullptr, &jerr);
-    ASSERT_EQ(jerr, nullptr) << "parse hook failed: " << jerr;
-    ASSERT_NE(hooks, nullptr);
+  hooks = oci_runtime_spec_hooks_parse_file(fname, nullptr, &jerr);
+  ASSERT_EQ(jerr, nullptr) << "parse hook failed: " << jerr;
+  ASSERT_NE(hooks, nullptr);
 
-    ASSERT_EQ(hooks->prestart_len, 1);
-    ASSERT_NE(hooks->prestart, nullptr);
-    EXPECT_STREQ(hooks->prestart[0]->path, "prestart.sh");
-    ASSERT_EQ(hooks->prestart[0]->args_len, 1);
-    EXPECT_STREQ(hooks->prestart[0]->args[0], "prestart.sh");
-    ASSERT_EQ(hooks->prestart[0]->env_len, 2);
-    EXPECT_STREQ(hooks->prestart[0]->env[0], "haozi=007");
-    EXPECT_STREQ(hooks->prestart[0]->env[1], "type=prestart");
-    ASSERT_EQ(hooks->prestart[0]->timeout, 1);
+  ASSERT_EQ(hooks->prestart_len, 1);
+  ASSERT_NE(hooks->prestart, nullptr);
+  EXPECT_STREQ(hooks->prestart[0]->path, "prestart.sh");
+  ASSERT_EQ(hooks->prestart[0]->args_len, 1);
+  EXPECT_STREQ(hooks->prestart[0]->args[0], "prestart.sh");
+  ASSERT_EQ(hooks->prestart[0]->env_len, 2);
+  EXPECT_STREQ(hooks->prestart[0]->env[0], "haozi=007");
+  EXPECT_STREQ(hooks->prestart[0]->env[1], "type=prestart");
+  ASSERT_EQ(hooks->prestart[0]->timeout, 1);
 
-    ASSERT_EQ(hooks->poststart_len, 1);
-    ASSERT_NE(hooks->poststart, nullptr);
-    EXPECT_STREQ(hooks->poststart[0]->path, "poststart.sh");
-    ASSERT_EQ(hooks->poststart[0]->args_len, 1);
-    EXPECT_STREQ(hooks->poststart[0]->args[0], "poststart.sh");
-    ASSERT_EQ(hooks->poststart[0]->env_len, 2);
-    EXPECT_STREQ(hooks->poststart[0]->env[0], "haozi=008");
-    EXPECT_STREQ(hooks->poststart[0]->env[1], "type=poststart");
-    ASSERT_EQ(hooks->poststart[0]->timeout, 2);
+  ASSERT_EQ(hooks->poststart_len, 1);
+  ASSERT_NE(hooks->poststart, nullptr);
+  EXPECT_STREQ(hooks->poststart[0]->path, "poststart.sh");
+  ASSERT_EQ(hooks->poststart[0]->args_len, 1);
+  EXPECT_STREQ(hooks->poststart[0]->args[0], "poststart.sh");
+  ASSERT_EQ(hooks->poststart[0]->env_len, 2);
+  EXPECT_STREQ(hooks->poststart[0]->env[0], "haozi=008");
+  EXPECT_STREQ(hooks->poststart[0]->env[1], "type=poststart");
+  ASSERT_EQ(hooks->poststart[0]->timeout, 2);
 
-    ASSERT_EQ(hooks->poststop_len, 1);
-    ASSERT_NE(hooks->poststop, nullptr);
-    EXPECT_STREQ(hooks->poststop[0]->path, "poststop.sh");
-    ASSERT_EQ(hooks->poststop[0]->args_len, 1);
-    EXPECT_STREQ(hooks->poststop[0]->args[0], "poststop.sh");
-    ASSERT_EQ(hooks->poststop[0]->env_len, 2);
-    EXPECT_STREQ(hooks->poststop[0]->env[0], "haozi=009");
-    EXPECT_STREQ(hooks->poststop[0]->env[1], "type=poststop");
-    ASSERT_EQ(hooks->poststop[0]->timeout, 3);
+  ASSERT_EQ(hooks->poststop_len, 1);
+  ASSERT_NE(hooks->poststop, nullptr);
+  EXPECT_STREQ(hooks->poststop[0]->path, "poststop.sh");
+  ASSERT_EQ(hooks->poststop[0]->args_len, 1);
+  EXPECT_STREQ(hooks->poststop[0]->args[0], "poststop.sh");
+  ASSERT_EQ(hooks->poststop[0]->env_len, 2);
+  EXPECT_STREQ(hooks->poststop[0]->env[0], "haozi=009");
+  EXPECT_STREQ(hooks->poststop[0]->env[1], "type=poststop");
+  ASSERT_EQ(hooks->poststop[0]->timeout, 3);
 
-    jstr = oci_runtime_spec_hooks_generate_json(hooks, nullptr, &jerr);
-    ASSERT_EQ(jerr, nullptr);
-    ASSERT_NE(jstr, nullptr);
+  jstr = oci_runtime_spec_hooks_generate_json(hooks, nullptr, &jerr);
+  ASSERT_EQ(jerr, nullptr);
+  ASSERT_NE(jstr, nullptr);
 
-    free_oci_runtime_spec_hooks(hooks);
-    free(jstr);
+  free_oci_runtime_spec_hooks(hooks);
+  free(jstr);
 }
 
-TEST(libocispec_testcase, test_json_readfile)
-{
-    const char *fname = "./ocihook.json";
-    const char *not_exist = "/tmp/not_exist.json";
-    char *jstr = nullptr;
-    size_t len = 0;
+TEST(libocispec_testcase, test_json_readfile) {
+  const char *fname = "./ocihook.json";
+  const char *not_exist = "/tmp/not_exist.json";
+  char *jstr = nullptr;
+  size_t len = 0;
 
-    jstr = read_file(fname, &len);
-    ASSERT_NE(jstr, nullptr);
-    ASSERT_EQ(len, 527);
-    free(jstr);
-    len = 0;
+  jstr = read_file(fname, &len);
+  ASSERT_NE(jstr, nullptr);
+  ASSERT_EQ(len, 527);
+  free(jstr);
+  len = 0;
 
-    jstr = read_file(not_exist, &len);
-    ASSERT_EQ(jstr, nullptr);
-    ASSERT_EQ(len, 0);
-    len = 0;
+  jstr = read_file(not_exist, &len);
+  ASSERT_EQ(jstr, nullptr);
+  ASSERT_EQ(len, 0);
+  len = 0;
 
-    jstr = read_file(nullptr, nullptr);
-    ASSERT_EQ(jstr, nullptr);
+  jstr = read_file(nullptr, nullptr);
+  ASSERT_EQ(jstr, nullptr);
 }
-
