a7bb4b084ed1f3809f6721e61d9529f2ce556a37
wujing <wujing50@huawei.com>
2021-09-27 10:24:29 +0800

    upgrade version to 2.0.6
    Signed-off-by: wujing <wujing50@huawei.com>

--- update-version.bash
+++ update-version.bash
@@ -55,7 +55,7 @@ old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print 
 first_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $1'})
 second_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $2'})
 third_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $3'})
-read -p "Which level version do you want to upgrade?[1/2/3/d/N](default:N)  select:" choice
+read -p "Which level version do you want to upgrade?[1/2/3/r/N](default:N)  select:" choice
 if [[ ! -n "${choice}" || ${choice} == "N" ]]; then
   echo "The version number has not been modified, it is still ${old_version}"
   exit 0
@@ -74,17 +74,14 @@ fi
 
 new_version=${first_old_version}.${second_old_version}.${third_old_version}
 
-if [[ ${choice} -ne "d" ]]; then
+if [[ ${choice} -ne "r" ]]; then
 	update_release_notes "$new_version"
 fi
 
 echo "The version number has been modified: ${old_version} => ${new_version}"
 
 old_release=$(cat ${specfile} | grep "%global" | grep "_release" | awk  {'print $3'})
-commit_id_long=`git log  --pretty=oneline  -1 | awk {'print $1'}`
-commit_id=${commit_id_long:0:8}
-new_release=`date "+%Y%m%d"`.`date "+%H%M%S"`.git$commit_id
-echo "The relase version  has been modified, it is ${new_release}"
+new_release=$(($old_release+1))
 sed -i "s/set(LCR_VERSION \"${old_version}\")/set(LCR_VERSION \"${new_version}\")/g" ${CMakefile}
 sed -i "s/^\%global _version ${old_version}$/\%global _version ${new_version}/g" ${specfile}
 sed -i "s/^\%global _release ${old_release}$/\%global _release ${new_release}/g" ${specfile}
