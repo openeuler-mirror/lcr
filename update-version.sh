0e8dd9d2cff6fffe76314e3f6b93053d478dc447
wujing <wujing50@huawei.com>
2021-11-16 15:29:02 +0800

    remove build platform restrictions
    Signed-off-by: wujing <wujing50@huawei.com>
    (cherry picked from commit 193ed9fd4e4345b96d2d3c6662cc59d789b5a433)

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+#
+# usage
+# ./update-version.sh
 #######################################################################
 # lcr: utils library for iSula
 #
@@ -21,18 +25,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 #######################################################################
-#!/bin/bash
-#
-# usage
-# ./update-version.bash
 topDir=$(git rev-parse --show-toplevel)
 specfile="${topDir}/lcr.spec"
-CMakefile="${topDir}/CMakeLists.txt"
 old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'})
 first_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $1'})
 second_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $2'})
 third_old_version=$(cat ${specfile} | grep "%global" | grep "_version" | awk  {'print $3'} | awk -F "." {'print $3'})
-read -p "Which level version do you want to upgrade?[1/2/3/d/N](default:N)  select:" choice
+read -p "Which level version do you want to upgrade?[1/2/3/r/N](default:N)  select:" choice
 if [[ ! -n "${choice}" || ${choice} == "N" ]]; then
   echo "The version number has not been modified, it is still ${old_version}"
   exit 0
@@ -51,15 +50,9 @@ fi
 
 new_version=${first_old_version}.${second_old_version}.${third_old_version}
 
-echo "The version number has been modified: ${old_version} => ${new_version}"
-
 old_release=$(cat ${specfile} | grep "%global" | grep "_release" | awk  {'print $3'})
-commit_id_long=`git log  --pretty=oneline  -1 | awk {'print $1'}`
-commit_id=${commit_id_long:0:8}
-new_release=`date "+%Y%m%d"`.`date "+%H%M%S"`.git$commit_id
-echo "The relase version  has been modified, it is ${new_release}"
-sed -i "s/set(LCR_VERSION \"${old_version}\")/set(LCR_VERSION \"${new_version}\")/g" ${CMakefile}
+new_release=$(($old_release+1))
 sed -i "s/^\%global _version ${old_version}$/\%global _version ${new_version}/g" ${specfile}
 sed -i "s/^\%global _release ${old_release}$/\%global _release ${new_release}/g" ${specfile}
 
-echo "The release number has been modified: ${old_release} => ${new_release}"
+echo "The release version has been modified: ${old_version}-${old_release} => ${new_version}-${new_release}"
