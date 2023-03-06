cd4ce196a2b144327f050269a14d8391a88e9920
zhongtao <zhongtao17@huawei.com>
2023-03-06 15:28:43 +0800

    adapt to repo of openeuler url changed
    Signed-off-by: zhongtao <zhongtao17@huawei.com>

--- CI/pr-gateway.sh
+++ CI/pr-gateway.sh
@@ -14,7 +14,15 @@
 ##- @Create: 2021-12-20
 #######################################################################
 
+sed -i "s#http://repo.openeuler.org#https://repo.huaweicloud.com/openeuler#g" /etc/yum.repos.d/openEuler.repo
+
+dnf update -y
+
 dnf install -y gtest-devel gmock-devel diffutils cmake gcc-c++ yajl-devel patch make libtool libevent-devel libevhtp-devel grpc grpc-plugins grpc-devel protobuf-devel libcurl libcurl-devel sqlite-devel libarchive-devel device-mapper-devel http-parser-devel libseccomp-devel libcap-devel libselinux-devel libwebsockets libwebsockets-devel systemd-devel git chrpath
+if [ $? -ne 0 ]; then
+    echo "install dependences failed"
+    exit 1
+fi
 
 cd ~
 
